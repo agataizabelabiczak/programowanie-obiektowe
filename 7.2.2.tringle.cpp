d8b7124ee35368b2f6febd15fd609e52da52f9eb
agataizabelabiczak <36738345+agataizabelabiczak@users.noreply.github.com>
2018-02-22 17:38:11 +0100

    Update 7.2.2.tringle.cpp

--- 7.2.2.tringle.cpp
+++ 7.2.2.tringle.cpp
@@ -6,7 +6,12 @@ int a, b, c;
 
 void f(Trojkat troj1, Trojkat *troj2){
 Trojkat troj1=Trojkat &troj2;
-    return troj2;
 }
-int main(){
+/*int main(){
+    Trojkat troj1={1, 4, 8};
+    Trojkat troj2;
+    f(troj1, troj2);
+    printf("%d", &troj2);
+    return 0;
+} */
     
